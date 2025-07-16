b550bafe2a504bb783e624723138796ca4d19237
avirajkhare00 <avirajkhare00@gmail.com>
2025-07-16 21:40:48 +0530

    added more codeeee

--- script.py
+++ script.py
@@ -6,6 +6,8 @@ def main():
   some_func()
   print("again calling some_func() below")
   some_func()
+  print("again calling some_func() below")
+  some_func()
 
 if __name__ == "__main__":
   main()
