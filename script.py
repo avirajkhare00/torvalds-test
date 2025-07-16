0b709648055462753de384c648f12ef8b9239fd7
avirajkhare00 <avirajkhare00@gmail.com>
2025-07-16 23:37:54 +0530

    added more statements

--- script.py
+++ script.py
@@ -4,6 +4,9 @@ def some_func():
 def main():
   print("calling some_func() below")
   some_func()
+  some_func()
+  print("this is some_func")
+  print("this is some_func")
 
 if __name__ == "__main__":
   main()
